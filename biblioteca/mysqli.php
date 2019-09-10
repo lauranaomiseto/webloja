f6eadbd842aa708a2cdbaf8bf15c495c334fe8e2
1831089 <1831089@LABBMAQ21.itp.local>
2019-09-10 12:34:21 -0300

    pra hospedar

--- biblioteca/mysqli.php
+++ biblioteca/mysqli.php
@@ -1,7 +1,7 @@
 <?php
 
 function conn() {
-    $cnx = mysqli_connect("localhost", "root", "", "webloja");
+    $cnx = mysqli_connect("sql111.epizy.com", "epiz_24452752", "KRVpTdOkgrkR", "epiz_24452752_webloja");
     if (!$cnx) die('Deu errado a conexao!');
     return $cnx;
 }
